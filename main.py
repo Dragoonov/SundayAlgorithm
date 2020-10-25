a096ca6bdc19527d308f831acdc69e6c0401d1c6
Dragoonov <dragovonnova@gmail.com>
2020-10-25 21:11:33 +0100

    Basic implementation

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def matches_at(t, p, w):
+    if len(t) < len(w):
+        return False
+    for i in range(len(w)):
+        if w[i] != t[p + i]:
+            return False
+    return True
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Złożność obliczeniowa:
+# czasowa: od 1 do |W | porównań znaków,
+# pamięciowa: O(1).
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+text = "Ala ma kota"
+pattern = "ma"
+
+print(matches_at(text, 3, pattern))
+
+
+################################
+
+def report(p):
+    print(f"Znaleziono na {p}")
+
+
+T = "ala ma kota i tez ma pamasa"
+W = "ma"
+lastp = {'a': -1, 'l': -1, ' ': -1, 'm': -1, 'k': -1, 'o': -1, 't': -1, 'i': -1, 'e': -1, 'z': -1, 'p': -1, 's': -1}
+for i in range(len(W)):
+    lastp[W[i]] = i
+
+p = 0
+while p <= len(T) - len(W):
+    if matches_at(T, p, W):
+        report(p)
+    p = p + len(W)
+    if p < len(T):
+        p = p - lastp[T[p]]
